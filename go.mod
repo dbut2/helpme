ee6ec65a6f810e438e4aaa4c67b64daa3591ec98
Dylan Butler <dylanbutler@outlook.com.au>
2023-04-26 15:16:37 +1000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dbut2/helpme
+
+go 1.20
+
+require github.com/sashabaranov/go-openai v1.9.0
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	args := strings.TrimSpace(strings.Join(os.Args[1:], " "))
+	if args == "" {
+		fmt.Println("no argument provided")
+		os.Exit(1)
+	}
+
+	openaiToken := os.Getenv("OPENAI_TOKEN")
+	if openaiToken == "" {
+		fmt.Println("env var not set")
+		os.Exit(1)
+	}
+
+	client := openai.NewClient(openaiToken)
+
+	resp, err := client.CreateChatCompletion(context.Background(), openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "You are a zsh based command line application that will return a command for the users need. You must only return the command to be ran. You must not supply an explanation. If the user requests multiple commands you should only return a single back.",
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: args,
+			},
+		},
+		Temperature: 0.1,
+	})
+
+	if err != nil {
+		fmt.Printf("ai error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	if len(resp.Choices) == 0 {
+		fmt.Println("no response")
+		os.Exit(1)
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
